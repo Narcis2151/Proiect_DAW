5a332a431f4030f03ed1c9b50695fcbe0ad8656a
Narcis2151 <fanica.narcis10@gmail.com>
2022-12-10 18:15:29 +0200

    Added Navbar

---
+++ Proiect_DAW/Controllers/FriendshipsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proiect_DAW.Data;
+using Proiect_DAW.Models;
+
+namespace Proiect_DAW.Controllers
+{
+    public class FriendshipsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public FriendshipsController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            db = context;
+
+            _userManager = userManager;
+
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var friendships = db.Friendships.Include("Requester").Include("Adresee");
+            ViewBag.Frienships = friendships;
+            return View();
+        }
+    }
+}
