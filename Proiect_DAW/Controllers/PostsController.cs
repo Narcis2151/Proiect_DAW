4ba504c920d4f28fbd83a4e9c3850b4f6c36015b
Narcis2151 <fanica.narcis10@gmail.com>
2023-01-09 19:36:30 +0200

    d

--- Proiect_DAW/Controllers/PostsController.cs
+++ Proiect_DAW/Controllers/PostsController.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Proiect_DAW.Data;
 using Proiect_DAW.Models;
+using System.Security.Claims;
 
 namespace Proiect_DAW.Controllers
 {
     public class PostsController : Controller
     {
-        private readonly ApplicationDbContext db;
+        private readonly ApplicationDbContext _db;
 
         private readonly UserManager<ApplicationUser> _userManager;
 
@@ -20,7 +21,7 @@ namespace Proiect_DAW.Controllers
             RoleManager<IdentityRole> roleManager
             )
         {
-            db = context;
+            _db = context;
 
             _userManager = userManager;
 
@@ -28,9 +29,48 @@ namespace Proiect_DAW.Controllers
         }
         public IActionResult Index()
         {
-            var posts = db.Posts.Include("ApplicationUser");
+            var posts = _db.Posts.OrderByDescending(x => x.CreateDate).Include("ApplicationUser").Include(p => p.Comments).ThenInclude(x => x.CreatorUser);
+            //foreach(Post post in posts)
+            //{
+            //    post.PostComments = _db.Comments.Where(x => x.PostId == post.Id).OrderByDescending(x => x.CreateDate).Include("CreatorUser").Select(x => new Comment
+            //    {
+            //        Id = x.Id,
+            //        Text = x.Text,
+            //        CreateDate = x.CreateDate,
+            //        CreatorUserId = x.CreatorUserId,
+            //        CreatorUser = x.CreatorUser,
+            //        PostId = x.PostId
+            //    }).ToList();
+            //}
             ViewBag.Posts = posts;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult _CreatePostPartial([Bind] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.CreateDate = DateTime.UtcNow;
+                post.Likes = 0;
+                post.ApplicationUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                _db.Posts.Add(post);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult _AddCommentPartial([Bind] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.CreatorUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                comment.CreateDate = DateTime.UtcNow;
+                _db.Comments.Add(comment);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
